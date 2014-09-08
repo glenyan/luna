3da0802b35f9d30b569c8a5dc86ab112a4830851
tomkrus <pzq0331@gmail.com>
2014-09-08 13:10:23 +0800

    init

---
+++ src/test/java/com/luna/common/plugin/service/TreeService.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.luna.common.plugin.service;
+
+import com.luna.common.plugin.entity.Tree;
+import com.luna.common.plugin.serivce.BaseTreeableService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-2-4 下午3:01
+ * <p>Version: 1.0
+ */
+@Service
+public class TreeService extends BaseTreeableService<Tree, Long> {
+
+}
